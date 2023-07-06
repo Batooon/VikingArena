aaf1bb0865d70125805fde765f2640523992e7ec
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-07 01:55:37 +0300

    ExitButton script implemented

---
+++ Assets/Code/UI/ExitButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UI
+{
+    public class ExitButton : MonoBehaviour
+    {
+        public Button Button;
+
+        private void Awake() => 
+            Button.onClick.AddListener(Quit);
+
+        private void Quit() => 
+            Application.Quit();
+    }
+}
