ba4b88ea69a52402689c17ba2ce0d6ffe3f6ce83
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-07 15:24:21 +0300

    basic hero movement implemented

---
+++ Assets/Code/Hero/HeroMove.cs
@@ -0,0 +1,40 @@
+﻿using Code.Infrastructure.Services;
+using Code.Infrastructure.Services.Input;
+using UnityEngine;
+
+namespace Code.Hero
+{
+    public class HeroMove : MonoBehaviour
+    {
+        public float MovementSpeed;
+        public CharacterController CharacterController;
+
+        private IInputService _inputService;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _inputService = AllServices.Container.Single<IInputService>();
+        }
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            Vector3 movementVector = Vector3.zero;
+            if (_inputService.Axis.sqrMagnitude > Constants.Epsilon)
+            {
+                movementVector = _camera.transform.TransformDirection(_inputService.Axis);
+                movementVector.y = 0;
+                movementVector.Normalize();
+            }
+
+            movementVector += Physics.gravity;
+
+            CharacterController.Move(movementVector * (MovementSpeed * Time.deltaTime));
+        }
+    }
+}
