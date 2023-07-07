4532f96d138a274e06df91ab9e5738fe80fdd0ed
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-08 00:47:24 +0300

    custom editors implemented

---
+++ Assets/Code/Editor/LevelStaticDataEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Code.Infrastructure.Services.StaticData;
+using Code.Logic.EnemySpawners;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Code.Editor
+{
+    [CustomEditor(typeof(LevelStaticData))]
+    public class LevelStaticDataEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            LevelStaticData levelData = (LevelStaticData)target;
+
+            if (GUILayout.Button("Collect Spawners"))
+            {
+                levelData.EnemySpawners =
+                    FindObjectsOfType<SpawnMarker>()
+                        .Select(x =>
+                            new EnemySpawnerData(x.MonsterTypeId, x.transform.position))
+                        .ToList();
+
+                levelData.LevelKey = SceneManager.GetActiveScene().name;
+            }
+
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
