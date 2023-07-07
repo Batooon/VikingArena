c337a13cf9c550a8abf0e69fd8a9bdb9b70ddb32
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-07 19:57:11 +0300

    Camera follow player implemented

---
+++ Assets/Code/Logic/Camera/CameraFollow.cs
@@ -0,0 +1,55 @@
+﻿using Code.Infrastructure.Services;
+using Code.Infrastructure.Services.Input;
+using UnityEngine;
+
+namespace Code.Logic.Camera
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public float TopRotationLimit;
+        public float BottomRotationLimit;
+        public float RotationSensitivity;
+        public float Distance;
+        public float OffsetY;
+        public Transform Following;
+
+        private IInputService _inputService;
+        private float _rotationX;
+
+        private void Awake()
+        {
+            _inputService = AllServices.Container.Single<IInputService>();
+            _rotationX = transform.eulerAngles.x;
+        }
+
+        private void LateUpdate()
+        {
+            if (Following == null)
+                return;
+            
+            Quaternion rotation = Following.rotation * Quaternion.Euler(VerticalRotation(), 0, 0);
+            var position = rotation * new Vector3(0, 0, -Distance) + FollowingPointPosition();
+
+            transform.SetPositionAndRotation(position, rotation);
+        }
+
+        public void Follow(GameObject following) =>
+            Following = following.transform;
+
+        private float VerticalRotation()
+        {
+            _rotationX -= _inputService.RotationAxis.x * RotationSensitivity;
+            _rotationX %= 360;
+            _rotationX = Mathf.Clamp(_rotationX, BottomRotationLimit, TopRotationLimit);
+            return _rotationX;
+        }
+
+        private Vector3 FollowingPointPosition()
+        {
+            Vector3 followingPosition = Following.position;
+            followingPosition.y += OffsetY;
+
+            return followingPosition;
+        }
+    }
+}
