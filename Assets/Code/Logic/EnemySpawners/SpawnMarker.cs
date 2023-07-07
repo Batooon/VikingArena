5e814bd1c36297e9860f5f04d427e60f9ec63be6
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-08 00:47:08 +0300

    EnemySpawners implemented

---
+++ Assets/Code/Logic/EnemySpawners/SpawnMarker.cs
@@ -0,0 +1,10 @@
+﻿using Code.Infrastructure.Services.StaticData;
+using UnityEngine;
+
+namespace Code.Logic.EnemySpawners
+{
+    public class SpawnMarker : MonoBehaviour
+    {
+        public MonsterTypeId MonsterTypeId;
+    }
+}
---
+++ Assets/Code/Logic/EnemySpawners/SpawnPoint.cs
@@ -0,0 +1,24 @@
+﻿using Code.Infrastructure.Factory;
+using Code.Infrastructure.Services.StaticData;
+using UnityEngine;
+
+namespace Code.Logic.EnemySpawners
+{
+    public class SpawnPoint : MonoBehaviour
+    {
+        public MonsterTypeId TypeId;
+
+        private IGameFactory _gameFactory;
+
+        public void Construct(IGameFactory gameFactory)
+        {
+            _gameFactory = gameFactory;
+            Spawn();
+        }
+
+        private void Spawn()
+        {
+            _gameFactory.CreateMonster(TypeId, at: transform);
+        }
+    }
+}
