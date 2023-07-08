8f21fa4d8bb8c8255325778253b3b5e5d4ab44c6
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-08 23:34:58 +0300

    UIFactory moved to its own file

---
+++ Assets/Code/UI/Services/Factory/UIFactory.cs
@@ -0,0 +1,44 @@
+﻿using Code.Infrastructure.Services.AssetManagement;
+using Code.Infrastructure.Services.Progress;
+using Code.Infrastructure.Services.StaticData;
+using Code.Infrastructure.States;
+using Code.UI.Windows;
+using UnityEngine;
+
+namespace Code.UI.Services.Factory
+{
+    public class UIFactory : IUIFactory
+    {
+        private readonly IAssets _assets;
+        private readonly IStaticDataService _staticData;
+        private readonly IProgressService _progressService;
+
+        private Transform _uiRoot;
+
+        public UIFactory(IAssets assets, IStaticDataService staticData, IProgressService progressService)
+        {
+            _assets = assets;
+            _staticData = staticData;
+            _progressService = progressService;
+        }
+
+        public void CreateMainMenu(GameStateMachine stateMachine) =>
+            CreateMenu(WindowId.MainMenu, stateMachine);
+
+        public void CreateEndGameMenu(GameStateMachine stateMachine) =>
+            CreateMenu(WindowId.EndGameMenu, stateMachine);
+
+        private void CreateMenu(WindowId windowId, GameStateMachine stateMachine)
+        {
+            WindowConfig config = _staticData.ForWindow(windowId);
+            WindowBase window = Object.Instantiate(config.Prefab, _uiRoot);
+
+            window.Construct(stateMachine, _progressService);
+        }
+
+        public void CreateUIRoot()
+        {
+            _uiRoot = _assets.Instantiate(AssetPath.UIRootPath).transform;
+        }
+    }
+}
