857f07a415c35d812f6731c21e9c238067a3cf5d
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-08 23:23:37 +0300

    HeroDeath implemented

---
+++ Assets/Code/Hero/HeroDeath.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Code.UI.Services.Factory;
+using Code.UI.Services.Windows;
+using UnityEngine;
+
+namespace Code.Hero
+{
+    public class HeroDeath : MonoBehaviour
+    {
+        public HeroHealth Health;
+        public HeroMove Move;
+        public HeroAttack Attack;
+        public HeroAnimator Animator;
+
+        public event Action Happened;
+
+        private bool _isDead;
+
+        private IWindowService _windowService;
+
+        public void Construct(IWindowService windowService) => 
+            _windowService = windowService;
+
+        private void Start()
+        {
+            Health.HealthChanged += OnHealthChanged;
+        }
+
+        private void OnHealthChanged()
+        {
+            if (ShouldDie())
+                Die();
+        }
+
+        private void Die()
+        {
+            _isDead = true;
+
+            Move.enabled = false;
+            Attack.enabled = false;
+            Animator.PlayDeath();
+            
+            _windowService.Open(WindowId.EndGameMenu);
+            
+            Happened?.Invoke();
+        }
+
+        private bool ShouldDie() => 
+            _isDead == false && Health.Current <= 0;
+    }
+}
