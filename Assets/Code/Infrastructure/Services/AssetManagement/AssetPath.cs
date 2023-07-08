98d56972710c2ef3c40e39db4d5ffa436ea8a616
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-08 23:23:59 +0300

    UI root and enemy spawner paths added

--- Assets/Code/Infrastructure/Services/AssetManagement/AssetPath.cs
+++ Assets/Code/Infrastructure/Services/AssetManagement/AssetPath.cs
@@ -4,5 +4,7 @@ namespace Code.Infrastructure.Services.AssetManagement
     {
         public const string SpawnerPath = "Enemies/SpawnPoint";
         public const string HudPath = "Hud/Hud";
+        public const string EnemyRespawner = "Enemies/Respawner";
+        public const string UIRootPath = "UI/UIRoot";
     }
 }
