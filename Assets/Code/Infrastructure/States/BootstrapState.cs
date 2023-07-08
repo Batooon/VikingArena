0d729c58408234129bb96358d5693a827cc1a735
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-08 23:37:56 +0300

    initializing main menu

--- Assets/Code/Infrastructure/States/BootstrapState.cs
+++ Assets/Code/Infrastructure/States/BootstrapState.cs
@@ -4,17 +4,20 @@ using Code.Infrastructure.Services.AssetManagement;
 using Code.Infrastructure.Services.Input;
 using Code.Infrastructure.Services.Progress;
 using Code.Infrastructure.Services.StaticData;
+using Code.UI.Services.Factory;
+using Code.UI.Services.Windows;
 
 namespace Code.Infrastructure.States
 {
     public class BootstrapState : IState
     {
-        private const string InitialSceneName = "Bootstrap";
-        private const string PlaySceneName = "Main";
+        private const string InitialSceneName = "Menu";
 
         private readonly GameStateMachine _stateMachine;
         private readonly SceneLoader _sceneLoader;
         private readonly AllServices _services;
+        private readonly IUIFactory _uiFactory;
+        private readonly IWindowService _windowService;
 
         public BootstrapState(GameStateMachine stateMachine, AllServices services, SceneLoader sceneLoader)
         {
@@ -23,16 +26,20 @@ namespace Code.Infrastructure.States
             _services = services;
 
             RegisterServices();
+
+            _uiFactory = _services.Single<IUIFactory>();
+            _windowService = _services.Single<IWindowService>();
         }
 
         public void Enter()
         {
-            _sceneLoader.Load(InitialSceneName, onLoaded: EnterLoadLevel);
+            _sceneLoader.Load(InitialSceneName, OnLoaded);
         }
 
-        private void EnterLoadLevel()
+        private void OnLoaded()
         {
-            _stateMachine.Enter<LoadLevelState, string>(PlaySceneName);
+            InitUIRoot();
+            InitMainMenu();
         }
 
         public void Exit()
@@ -48,11 +55,20 @@ namespace Code.Infrastructure.States
 
             RegisterProgress();
 
+            _services.RegisterSingle<IUIFactory>(new UIFactory(
+                _services.Single<IAssets>(),
+                _services.Single<IStaticDataService>(),
+                _services.Single<IProgressService>()
+            ));
+
+            _services.RegisterSingle<IWindowService>(new WindowService(_stateMachine, _services.Single<IUIFactory>()));
+
             _services.RegisterSingle<IGameFactory>(new GameFactory(
                 _services.Single<IAssets>(),
                 _services.Single<IStaticDataService>(),
                 _services.Single<IProgressService>(),
-                _services.Single<IInputService>()
+                _services.Single<IInputService>(),
+                _services.Single<IWindowService>()
             ));
         }
 
@@ -67,14 +83,13 @@ namespace Code.Infrastructure.States
         {
             IProgressService progressService = new ProgressService();
             _services.RegisterSingle(progressService);
-            progressService.Progress = new PlayerProgress
-            {
-                HeroStats =
-                {
-                    Damage = 1,
-                    Cleavage = .5f,
-                }
-            };
+            progressService.Progress = new PlayerProgress();
         }
+
+        private void InitUIRoot() =>
+            _uiFactory.CreateUIRoot();
+
+        private void InitMainMenu() =>
+            _windowService.Open(WindowId.MainMenu);
     }
 }
