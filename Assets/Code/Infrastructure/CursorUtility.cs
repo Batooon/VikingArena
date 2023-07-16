942de835c0f9b0a65970b3ec9542466f9f245117
Anton <47085893+Batooon@users.noreply.github.com>
2023-07-17 00:44:21 +0300

    CursorUtility added

---
+++ Assets/Code/Infrastructure/CursorUtility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Code.Infrastructure
+{
+    public static class CursorUtility
+    {
+        public static void ShowAndUnlock()
+        {
+            SetCursorState(true);
+        }
+        
+        public static void HideAndLock()
+        {
+            SetCursorState(false, CursorLockMode.Locked);
+        }
+
+        private static void SetCursorState(bool active, CursorLockMode lockMode = CursorLockMode.None)
+        {
+            Cursor.visible = active;
+            Cursor.lockState = lockMode;
+        }
+    }
+}
